d7279cf5fd21d69b38b7b754df8fae7697e14867
Shapovalov Dmitry <mr.demon4ik@gmail.com>
2024-08-27 19:11:19 +0300

    Сделал реализацию на JDBC и HIBERNATE

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,118 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
 
+    private static final String CREATE_TABLE_SQL = """
+            CREATE TABLE IF NOT EXISTS "user"
+            (
+                id       SERIAL PRIMARY KEY,
+                name     VARCHAR(128),
+                last_name VARCHAR(128),
+                age      SMALLINT
+            );
+            """;
+
+    private static final String DROP_TABLE_SQL = """
+            DROP TABLE IF EXISTS "user";
+            """;
+
+    private static final String SAVE_USER_SQL = """
+            INSERT INTO "user"(name, last_name, age)
+            VALUES (?, ?, ?)
+            """;
+
+    private static final String REMOVE_USER_BY_ID_SQL = """
+             DELETE FROM "user" 
+             WHERE id = ?;
+            """;
+
+    private static final String GET_ALL_USERS_SQL = """
+            SELECT id,
+                   name,
+                   last_name,
+                   age
+                   FROM "user";
+            """;
+
+    private static final String CLEAN_USERS_TABLE_SQL = """
+            DELETE FROM "user";
+            """;
+
+    public UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
-
+        try (Connection connection = Util.connect();
+             PreparedStatement preparedStatement = connection.prepareStatement(CREATE_TABLE_SQL)) {
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void dropUsersTable() {
-
+        try (Connection connection = Util.connect();
+             PreparedStatement preparedStatement = connection.prepareStatement(DROP_TABLE_SQL)) {
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Connection connection = Util.connect();
+             PreparedStatement preparedStatement = connection.prepareStatement(SAVE_USER_SQL)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.execute();
+            System.out.println("User с именем – "+name+" добавлен в базу данных");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void removeUserById(long id) {
-
+        try (Connection connection = Util.connect();
+             PreparedStatement preparedStatement = connection.prepareStatement(REMOVE_USER_BY_ID_SQL)) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        try (Connection connection = Util.connect();
+             PreparedStatement preparedStatement = connection.prepareStatement(GET_ALL_USERS_SQL)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                User user = new User(resultSet.getString("name"),
+                        resultSet.getString("last_name"),
+                        resultSet.getByte("age"));
+                user.setId(resultSet.getLong("id"));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
-
+        try (Connection connection = Util.connect();
+             PreparedStatement preparedStatement = connection.prepareStatement(CLEAN_USERS_TABLE_SQL)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,44 @@
 package jm.task.core.jdbc.util;
 
-public class Util {
-    // реализуйте настройку соеденения с БД
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public final class Util {
+
+    private static final String PASSWORD_KEY = "db.password";
+    private static final String USERNAME_KEY = "db.username";
+    private static final String URL_KEY = "db.url";
+
+    private Util(){}
+    // JDBC
+    public static Connection connect(){
+        try {
+            return DriverManager.getConnection(
+                    PropertiesUtil.get(URL_KEY),
+                    PropertiesUtil.get(USERNAME_KEY),
+                    PropertiesUtil.get(PASSWORD_KEY));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    // HIBERNATE
+    public static SessionFactory userSessionFactory(){
+        Properties properties = new Properties();
+        properties.put(Environment.USER, PropertiesUtil.get(USERNAME_KEY));
+        properties.put(Environment.URL, PropertiesUtil.get(URL_KEY));
+        properties.put(Environment.PASS, PropertiesUtil.get(PASSWORD_KEY));
+        properties.put(Environment.DRIVER, "org.postgresql.Driver");
+//        properties.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+        properties.put(Environment.SHOW_SQL, "true");
+        properties.put(Environment.FORMAT_SQL, "true");
+        return new Configuration().setProperties(properties).addAnnotatedClass(User.class).buildSessionFactory();
+
+    }
 }
